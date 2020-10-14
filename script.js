4f0401d1c3c82ca281e2ac5acb59c2f62be8ad45
olonafrancois <francois.olona@hetic.net>
2020-10-14 15:54:55 +0200

    Finished

--- script.js
+++ script.js
@@ -166,26 +166,24 @@ valider.addEventListener('click',nextquestion)
 
 oui.addEventListener('click', () =>
 {
-    oui.style.boxShadow = '0px 0px 5px 0px rgba(6,45,91,1)'
+    oui.style.border = 'solid 0.5vh #48fa65'
     oui.style.backgroundColor = '#00ff91'
-    non.style.boxShadow = '0px 2px 5px 0px rgba(6,45,91,1)'
+    non.style.border = 'solid 0.5vh #062d5b'
     non.style.backgroundColor = '#2ee596'
 })
 non.addEventListener('click', () =>
 {
-    non.style.boxShadow = '0px 0px 5px 0px rgba(6,45,91,1)'
+    non.style.border = 'solid 0.5vh #48fa65'
     non.style.backgroundColor = '#00ff91'
-    oui.style.boxShadow = '0px 2px 5px 0px rgba(6,45,91,1)'
+    oui.style.border = 'solid 0.5vh #062d5b'
     oui.style.backgroundColor = '#2ee596'
 })
 valider.addEventListener('click', () =>
 {
-    valider.style.boxShadow = '0px 0px 5px 0px rgba(6,45,91,1)'
     valider.style.backgroundColor = '#00ff91'
-    oui.style.boxShadow = '0px 2px 5px 0px rgba(6,45,91,1)'
     oui.style.backgroundColor = '#2ee596'
-    non.style.boxShadow = '0px 2px 5px 0px rgba(6,45,91,1)'
+    oui.style.border = 'solid 0.5vh #062d5b'
     non.style.backgroundColor = '#2ee596'
-    valider.style.boxShadow = '0px 2px 5px 0px rgba(6,45,91,1)'
+    non.style.border = 'solid 0.5vh #062d5b'
     valider.style.backgroundColor = '#2ee596'
 })
